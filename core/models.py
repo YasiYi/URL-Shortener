9c829e7561bf475d1855c91d825a1861b21578b6
YasiYi <yasi.yousefi96@gmail.com>
2025-09-07 17:59:23 +0330

    Add User and URL models.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extrafields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    fullname = models.CharField(max_length=150)
+    created_at = models.DateField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
+class URL(models.Model):
+    original_url = models.URLField()
+    short_url = models.CharField(max_length=100, unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='urls')
+    created_at = models.DateTimeField(auto_now_add=True)
+    enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.short_url
